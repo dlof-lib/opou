861247e81c6cba8fbc10d4c7e0f64888e5fe98f1
dlof-lib <javadlof8@gmail.com>
2026-08-23 15:58:42 +0300

    Initial commit from ZIP upload

---
+++ app/src/main/java/com/OPEN/OU/ui/theme/Color.kt
@@ -0,0 +1,14 @@
+package com.OPEN.OU.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val OpouGreen = Color(0xFF0B7A4A)
+val OpouGreenDark = Color(0xFF095E39)
+val OpouGreenLight = Color(0xFF3FA873)
+val OpouGold = Color(0xFFE4B94A)
+val OpouBackground = Color(0xFFF5F8F6)
+val OpouSurface = Color(0xFFFFFFFF)
+val OpouTextPrimary = Color(0xFF122019)
+val OpouTextSecondary = Color(0xFF5B6B62)
+val OpouStar = Color(0xFFF5A623)
+val OpouBrokenHeart = Color(0xFFE23E57)
